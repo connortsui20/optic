c3686aec9a09b73d5e5d3d6afb25af7aaada4b9c
Connor Tsui <connor.tsui20@gmail.com>
2026-08-18 13:00:25 -0400

    Create cargo_optic package
    Signed-off-by: Connor Tsui <connor.tsui20@gmail.com>

---
+++ crates/cargo_optic/src/lib.rs
@@ -0,0 +1,4 @@
+//! Application library for `cargo_optic`.
+//!
+//! This crate will own the Optic project store, capture history, queries, comparisons, and client
+//! state. The `cargo_optic` binary and terminal interface will use this library.
